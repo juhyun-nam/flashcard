d86a5a4187a1548de865bb755967804c6abb466b
nam <nothinis@naver.com>
2020-08-16 00:20:25 +0900

    Updates

---
+++ precache-manifest.5b0432afa0722d11981aef2e1e0db872.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86875413f2cd2ddea8e44590f62255e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b33f64a08247a6e0df1",
+    "url": "./static/js/2.40ebac04.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.40ebac04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0214716bc02fa098bb9",
+    "url": "./static/js/main.cdd9f95e.chunk.js"
+  },
+  {
+    "revision": "f3b638aea2f4b43effa5",
+    "url": "./static/js/runtime-main.1f14ffc2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./docs/precache-manifest.9657e0d304b87843f69615eade8713c4.js"
+  "./precache-manifest.5b0432afa0722d11981aef2e1e0db872.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./docs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
