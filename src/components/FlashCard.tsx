1b69ffa57354535f7c1445802fa22772631a4bb6
juhyun_nam <juhyun_nam@tmax.co.kr>
2020-09-02 07:47:03 +0900

    separate flash card

--- src/components/FlashCard.tsx
+++ src/components/FlashCard.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -6,34 +6,27 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 // import Flip from 'react-card-flip';
 
-type FlipProp = {
-  flipped: boolean,
-  setFlip: Dispatch<boolean>,
+type FlipCardProp = {
   title: string,
-  front: string,
-  back: string,
+  subtitle: string,
+  content: string,
 };
 
-function Flip({ flipped, setFlip, title, front, back }: FlipProp) {
+function FlipCard({ title, subtitle, content }: FlipCardProp) {
   const cardStyle = {
     width: '100%',
     minHeight: '100%',
   };
-  const renderCard = (key: string, content: string) => (
-    <Card style={cardStyle} elevation={24} key={key} onClick={() => { setFlip(!flipped) }}>
-      <CardHeader title={title} subheader={key} />
+  // const renderCard = (key: string, content: string) => 
+  return (
+    <Card style={cardStyle} elevation={24}>
+      <CardHeader title={title} subheader={subtitle} />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
           {content}
         </Typography>
       </CardContent>
     </Card>
-  );
-
-  return (
-    <>
-      {!flipped ? renderCard('front', front) : renderCard('back', back)}
-    </>
   );
 }
 
@@ -48,10 +41,12 @@ export default function FlashCard({ title, front, back }: FlashCardProp) {
   useEffect(() => {
     setFlip(false);
   }, [front]);
+  const frontCard = { title, subtitle: 'front', content: front };
+  const backCard = { title, subtitle: 'back', content: back };
 
   return (
-    <Box height="100%">
-      <Flip flipped={flipped} setFlip={setFlip} title={title} front={front} back={back} />
+    <Box height="100%" onClick={() => { setFlip(!flipped) }}>
+      {flipped ? FlipCard(backCard) : FlipCard(frontCard)}
     </Box>
   );
 }
